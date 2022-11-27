649c679638d84de75514e31c1f874b03c36b858f
Maciel Ribeiro de Oliveira <38005099+Maxnegrao33@users.noreply.github.com>
2022-11-27 11:00:11 -0300

    Add files via upload

---
+++ String.js
@@ -0,0 +1,19 @@
+let nome = "Wilson";
+let sobrenome = "Simonal";
+
+//let nomeCompleto = nome + " " + sobrenome;
+    //console.log(nomeCompleto);
+    
+//Isto é uma operação de concatenação com Strings
+
+//Template String é uma string que permite operações, que usa estas aspas ``
+//Uma expressão
+let nomeCompleto = `${nome} ${sobrenome}`;
+    console.log("Nome Completo:" + nomeCompleto);
+
+let idade = 90;
+let idadeString = `Idade: ${idade} anos`;
+    console.log(idadeString);
+
+
+
